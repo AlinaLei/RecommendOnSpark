ff5c4c3aec779839a21b7038728f271ed8d4488e
AlinaLei <573493657@qq.com>
2019-07-02 17:40:37 +0800

    modify hive

--- recommend_train.py
+++ recommend_train.py
@@ -263,7 +263,7 @@ if __name__ == "__main__":
 
             try:
                 #Hive相关操作
-                result.rdd.map(lambda l:Row(str(l.user)+"|"+str(l.products)+"|"+str(l.rating)+"|"+str(l.category)+"|"+str(l.channel))).toDF(["user","products","rating","category","channel"]).registerTempTable("result_tmp")
+                result.rdd.map(lambda l:Row(user=str(l.user),products=str(l.products),rating=str(l.rating),category=str(l.category),channel=str(l.channel))).toDF(["user","products","rating","category","channel"]).registerTempTable("result_tmp")
                 hive_context = HiveContext(sc)
                 hive_result = hive_context.sql( """select * from result_tmp limit 10""")
                 hive_result.show()
