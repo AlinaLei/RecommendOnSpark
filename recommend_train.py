c23c1a959663d12dd36415d9fc39e12c20ad3416
AlinaLei <573493657@qq.com>
2019-07-04 18:05:31 +0800

    fix the import

--- recommend_train.py
+++ recommend_train.py
@@ -1,9 +1,9 @@
 #/usr/bin/env python3
 # -*-coding:utf:8 -*-
 
-from .models.model_feature import *
+from models import model_feature
 from config.config import *
-from .data.data_handle import *
+from data import data_handle
 
 ##TODO 拆分结果集
 ##TODO 将拆分结果保存到文件中
@@ -13,26 +13,26 @@ if __name__ == "__main__":
 
     #训练模型
     sc = CreateSparkContext()
-    raw_ratings_rdd =read_file_to_RDD(sc,"/data/lin/train_data/user_data/part-00000-fa8d558c-15be-4399-a575-f0a5391c46f9-c000.csv")
-    ratings_rdd = handle_read_data(raw_ratings_rdd,3)
+    raw_ratings_rdd =data_handle.read_file_to_RDD(sc,"/data/lin/train_data/user_data/part-00000-fa8d558c-15be-4399-a575-f0a5391c46f9-c000.csv")
+    ratings_rdd = data_handle.handle_read_data(raw_ratings_rdd,3)
     try:
-        ratings_datas = create_als_data(ratings_rdd)
-        training_ratings, testing_ratings = split_train_test_data(ratings_datas)
-        model, rmse_value, rank, iterations, lambda_=train_model_evaluate(training_ratings, testing_ratings, 10, 4, 0.0001)
+        ratings_datas = model_feature.create_als_data(ratings_rdd)
+        training_ratings, testing_ratings = model_feature.split_train_test_data(ratings_datas)
+        model, rmse_value, rank, iterations, lambda_=model_feature.train_model_evaluate(training_ratings, testing_ratings, 10, 4, 0.0001)
         try:
             #save_model(sc, model, "file:///data/lin/savemodel/als_model_test")
 
             print("start recommned result")
-            recommend = Recommend(ALS_model=model)
-            recommendation_all = recommend.map(hadle_result).toDF()
-            category = read_file_to_RDD(sc, "/data/lin/train_data/user_data/category.txt")
-            catrgory_rdd = handle_read_data(category, 3,sep =',')
-            category_df = transform_rdd_to_DF(catrgory_rdd, ['products','category','channel'])
-            result = handle_DataFrame(recommendation_all, category_df,'products')
-            split_data_by_category(result, 'category', "/data/lin/predict_data/recommend_movie_result/test",mode='overwrite')
+            recommend = model_feature.Recommend(ALS_model=model)
+            recommendation_all = recommend.map(data_handle.hadle_result).toDF()
+            category = data_handle.read_file_to_RDD(sc, "/data/lin/train_data/user_data/category.txt")
+            catrgory_rdd = data_handle.handle_read_data(category, 3,sep =',')
+            category_df = data_handle.transform_rdd_to_DF(catrgory_rdd, ['products','category','channel'])
+            result = data_handle.handle_DataFrame(recommendation_all, category_df,'products')
+            data_handle.split_data_by_category(result, 'category', "/data/lin/predict_data/recommend_movie_result/test",mode='overwrite')
 
             print("the result head is :{}".format(result.show(4)))
-            save_DF(result, "/data/lin/predict_data/recommend_movie_result/test/category_result")
+            data_handle.save_DF(result, "/data/lin/predict_data/recommend_movie_result/test/category_result")
             #save_DF(result.rdd.map(lambda l:Row(str(l.user)+"|"+str(l.products)+"|"+str(l.rating)+"|"+str(l.category)+"|"+str(l.channel))).toDF(), "/data/lin/predict_data/recommend_movie_result/test")
             #result.toDF(['user','products','rating','category','channel']).registerTempTable("result_tmp")
             """
