e05250ccc5231737c1675a81046ca959be20fa53
AlinaLei <573493657@qq.com>
2019-07-05 14:16:26 +0800

    resolve import error 35

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import os
-
+import sys
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 
 os.system(" /opt/spark/bin/spark-submit /data/lin/code/code_git/RecommendOnSpark/recommend_train.py")
 
